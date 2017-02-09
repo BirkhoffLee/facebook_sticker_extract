d83ea17a71fb08335e835b168a74e2af0e469a37
BirkhoffLee <github@birkhoff.me>
2017-02-10 02:23:40 +0800

    Clearify the algorithm

--- extract.py
+++ extract.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+import os
+from PIL import Image
+
 cols = [4, 4, 4, 3]
 outputDirFull = "./output/full/"
 outputDirSticker = "./output/sticker/"
@@ -7,8 +10,11 @@ outputDirCustom = "./output/custom/"
 
 ###
 
-from PIL import Image
-import os
+sticker_width = 288
+sticker_height = 288
+sticker_offset = 24
+
+###
 
 if not os.path.exists(outputDirFull):
     os.makedirs(outputDirFull)
@@ -21,10 +27,13 @@ if not os.path.exists(outputDirCustom):
 img = Image.open("trash_doves.png")
 count = 0
 
-for colIndex in range(len(cols)):
+for colIndex in enumerate(cols):
     for rowIndex in range(cols[colIndex]):
         count += 1
-        cropped = img.crop((24 + 288 * rowIndex, 24 + 288 * colIndex, 288 * (rowIndex + 1) - 24, 288 * (colIndex + 1) - 24)) # 240x240
+        cropped = img.crop((sticker_offset + sticker_width * rowIndex,
+                            sticker_offset + sticker_height * colIndex,
+                            sticker_width * (rowIndex + 1) - sticker_offset,
+                            sticker_height * (colIndex + 1) - sticker_offset))
 
         # Full-sized version
         cropped.save(outputDirFull + str(count) + ".png")
@@ -38,8 +47,11 @@ for colIndex in range(len(cols)):
         # Customized-sized version
         wantedDaveSize = (100, 100)
         wantedImageSize = (180, 180)
+
+        offset_width = (wantedImageSize[0] - wantedDaveSize[0]) / 2
+        offset_height = (wantedImageSize[1] - wantedDaveSize[1]) / 2
         cropped.thumbnail(wantedDaveSize)
         new = Image.new("RGBA", wantedImageSize, (0, 0, 0, 0))
-        new.paste(cropped, ((wantedImageSize[0] - wantedDaveSize[0]) / 2, (wantedImageSize[1] - wantedDaveSize[1]) / 2))
+        new.paste(cropped, (offset_width, offset_height))
         new.save(outputDirCustom + str(count) + ".png")
         ####
