3777dc816b3d3201fdad16c6d353a19f8190b37f
BirkhoffLee <github@birkhoff.me>
2017-02-10 03:04:49 +0800

    Mark settings're in px 'n arrange custom size up

--- extract.py
+++ extract.py
@@ -9,11 +9,15 @@ outputDirFull = "./output/full/"
 outputDirSticker = "./output/sticker/"
 outputDirCustom = "./output/custom/"
 
+# Custom-sized
+wantedImageSize = (180, 180)    # The actual image size (px)
+wantedStickerSize = (100, 100)  # The sticker's size in the actual image (px)
+
 ###
 
-sticker_width = 288
-sticker_height = 288
-sticker_offset = 24
+sticker_width = 288 # (px)
+sticker_height = 288 # (px)
+sticker_offset = 24 # (px)
 
 ###
 
@@ -38,21 +42,15 @@ for colIndex, row in enumerate(table):
 
         # Full-sized version
         cropped.save(outputDirFull + str(count) + ".png")
-        ####
 
         # Sticker-sized version
         cropped.thumbnail((120, 120))
         cropped.save(outputDirSticker + str(count) + ".png")
-        ####
 
-        # Customized-sized version
-        wantedDaveSize = (100, 100)
-        wantedImageSize = (180, 180)
-
-        offset_width = (wantedImageSize[0] - wantedDaveSize[0]) / 2
-        offset_height = (wantedImageSize[1] - wantedDaveSize[1]) / 2
-        cropped.thumbnail(wantedDaveSize)
+        # Custom-sized version
+        offset_width = (wantedImageSize[0] - wantedStickerSize[0]) / 2
+        offset_height = (wantedImageSize[1] - wantedStickerSize[1]) / 2
+        cropped.thumbnail(wantedStickerSize)
         new = Image.new("RGBA", wantedImageSize, (0, 0, 0, 0))
         new.paste(cropped, (offset_width, offset_height))
         new.save(outputDirCustom + str(count) + ".png")
-        ####
