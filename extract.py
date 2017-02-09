d2e133c0c0249229cd33b9fa60ba408fd42d8f55
BirkhoffLee <github@birkhoff.me>
2017-02-10 01:47:37 +0800

    Better performance

--- extract.py
+++ extract.py
@@ -17,8 +17,5 @@ if not os.path.exists(outputDir):
 for index in range(len(cols)):
     for _ in range(cols[index]):
         count += 1
-        cropped = img.crop((12 + 144 * _, 12 + 144 * index, 144 * (_ + 1) - 12, 144 * (index + 1) - 12))
-        (width, height) = cropped.size
-        new = Image.new("RGBA", (width, height), (0, 0, 0, 0))
-        new.paste(cropped)
-        new.save(outputDir + "/" + str(count) + ".png")
+        cropped = img.crop((12 + 144 * _, 12 + 144 * index, 144 * (_ + 1) - 12, 144 * (index + 1) - 12)) # 120x120
+        cropped.save(outputDir + "/" + str(count) + ".png")
