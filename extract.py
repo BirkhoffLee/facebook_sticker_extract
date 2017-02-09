192676f956e613231e3fb94ba6f5c2605f048fef
BirkhoffLee <github@birkhoff.me>
2017-02-10 01:59:59 +0800

    full or scale

--- extract.py
+++ extract.py
@@ -18,4 +18,16 @@ for index in range(len(cols)):
     for _ in range(cols[index]):
         count += 1
         cropped = img.crop((12 + 144 * _, 12 + 144 * index, 144 * (_ + 1) - 12, 144 * (index + 1) - 12)) # 120x120
-        cropped.save(outputDir + "/" + str(count) + ".png")
+
+        # If you want the full version
+        # cropped.save(outputDir + "/" + str(count) + ".png")
+        ####
+
+        # If you want a smaller version
+        wantedDaveSize = (100, 100)
+        wantedImageSize = (180, 180)
+        cropped.thumbnail(wantedDaveSize)
+        new = Image.new("RGBA", wantedImageSize, (0, 0, 0, 0))
+        new.paste(cropped, ((wantedImageSize[0] - wantedDaveSize[0]) / 2, (wantedImageSize[1] - wantedDaveSize[1]) / 2))
+        new.save(outputDir + "/" + str(count) + ".png")
+        ####
